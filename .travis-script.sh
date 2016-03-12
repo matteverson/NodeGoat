174302d92fc076d56c5d5049832b020a2abf8ded
Matt Everson <ti.just.me@gmail.com>
2016-03-11 23:29:15 -0600

    Update .travis-script.sh

--- .travis-script.sh
+++ .travis-script.sh
@@ -4,4 +4,5 @@ sleep 5
 curl http://127.0.0.1:4000/login
 hostip=$(docker run -u zap -it owasp/zap2docker-stable bash -c 'ip route show' | awk '/default/ {print$3}')
 echo Scanning $hostip
-docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" --spider -r http://$hostip:4000
+docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained \
+-o "-config api.disablekey=true" --spider -r http://$hostip:4000
